564ac5ceaf52b4e7b687394e447aba4cb301f273
Tran Dang Tin <thantienbut@gmail.com>
2018-11-26 23:12:19 +0700

    update

---
+++ QuanLyNhaThuoc/QuanLyNhaThuoc/HoaDonBanThuocFrm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyNhaThuoc
+{
+    public partial class HoaDonBanThuocFrm : Form
+    {
+        private HoaDonBanDB hoaDonBanDB;
+        private string id;
+        public HoaDonBanThuocFrm()
+        {
+            hoaDonBanDB = new HoaDonBanDB();
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            loadDB();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HoaDonBan hoaDonBan = new HoaDonBan();
+            hoaDonBan.nguoiMua = txtNguoiMua.Text;
+            hoaDonBan.tenThuoc = txtTenThuoc.Text;
+            hoaDonBan.maThuoc = txtMaThuoc.Text;
+            hoaDonBan.soLuong = Int32.Parse(txtSoLuong.Text);
+            hoaDonBan.donGia = Int32.Parse(txtDonGia.Text);
+            hoaDonBanDB.insert(hoaDonBan);
+            loadDB();
+        }
+
+        private void loadDB()
+        {
+            DataSet ds = hoaDonBanDB.getAll();
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            HoaDonBan hoaDonBan = new HoaDonBan();
+            hoaDonBan.id = long.Parse(id);
+            hoaDonBan.nguoiMua = txtNguoiMua.Text;
+            hoaDonBan.tenThuoc = txtTenThuoc.Text;
+            hoaDonBan.maThuoc = txtMaThuoc.Text;
+            hoaDonBan.soLuong = Int32.Parse(txtSoLuong.Text);
+            hoaDonBan.donGia = Int32.Parse(txtDonGia.Text);
+            hoaDonBanDB.update(hoaDonBan);
+            loadDB();
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                id = row.Cells[0].Value.ToString();
+                txtNguoiMua.Text = row.Cells[1].Value.ToString();
+                txtTenThuoc.Text = row.Cells[2].Value.ToString();
+                txtMaThuoc.Text = row.Cells[3].Value.ToString();
+                txtSoLuong.Text = row.Cells[4].Value.ToString();
+                txtDonGia.Text = row.Cells[5].Value.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            HoaDonBan hoaDonBan = new HoaDonBan();
+            hoaDonBan.id = long.Parse(id);
+            hoaDonBanDB.delete(hoaDonBan);
+            txtNguoiMua.Text = "";
+            txtTenThuoc.Text = "";
+            txtMaThuoc.Text = "";
+            txtSoLuong.Text = "";
+            txtDonGia.Text = "";
+            loadDB();
+        }
+    }
+}
---
+++ QuanLyNhaThuoc/QuanLyNhaThuoc/MainMenu.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace QuanLyNhaThuoc
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnThongKe = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnBanThuoc = new System.Windows.Forms.Button();
+            this.btnNhapThuoc = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.btnThongKe, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnBanThuoc, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnNhapThuoc, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(120, 67);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(420, 250);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnThongKe
+            // 
+            this.btnThongKe.Location = new System.Drawing.Point(3, 128);
+            this.btnThongKe.Name = "btnThongKe";
+            this.btnThongKe.Size = new System.Drawing.Size(192, 108);
+            this.btnThongKe.TabIndex = 3;
+            this.btnThongKe.Text = "Thống Kê";
+            this.btnThongKe.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(213, 128);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(192, 108);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnBanThuoc
+            // 
+            this.btnBanThuoc.Location = new System.Drawing.Point(3, 3);
+            this.btnBanThuoc.Name = "btnBanThuoc";
+            this.btnBanThuoc.Size = new System.Drawing.Size(192, 108);
+            this.btnBanThuoc.TabIndex = 0;
+            this.btnBanThuoc.Text = "Quản Lý Hóa Đơn Bán Thuốc";
+            this.btnBanThuoc.UseVisualStyleBackColor = true;
+            this.btnBanThuoc.Click += new System.EventHandler(this.btnBanThuoc_Click);
+            // 
+            // btnNhapThuoc
+            // 
+            this.btnNhapThuoc.Location = new System.Drawing.Point(213, 3);
+            this.btnNhapThuoc.Name = "btnNhapThuoc";
+            this.btnNhapThuoc.Size = new System.Drawing.Size(192, 108);
+            this.btnNhapThuoc.TabIndex = 1;
+            this.btnNhapThuoc.Text = "Quản Lý Hóa Đơn Nhập Thuốc";
+            this.btnNhapThuoc.UseVisualStyleBackColor = true;
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 396);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Quản lý nhà thuốc";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnBanThuoc;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnNhapThuoc;
+        private System.Windows.Forms.Button btnThongKe;
+    }
+}
